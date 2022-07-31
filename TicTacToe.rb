c76ee5bca7eec82b214ba1f925960d266f96228b
HaoZ124 <zhouhao927@gmail.com>
2022-07-31 12:21:33 -0600

    Compiled the module and pasted the test

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -1,64 +1,69 @@
-LINES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-
-col_seperator, row_seperator = ' | ', '--+---+----'
-board = [[1,2,3], [4,5,6], [7,8,9]]
-puts board.map { |row| row.join(col_seperator) }.join("\n" + row_seperator + "\n")
-
-
-isGameOver = false
-play_count = 0
-played_board = board
-player = 1
-positions_1 = []
-positions_2 = []
-while !isGameOver && play_count < 9 do
-    if player == 1
-        begin
-        puts 'Player 1 Please Enter The Number To Mark'
-        choice_1 = gets.chomp.to_i
-        end while (positions_1+positions_2).include?(choice_1)
-        positions_1 << choice_1
-        p positions_1
-        played_board = played_board.map do |row| 
-            row.map {|cell| cell == choice_1&&cell != 'X'&&cell != 'O' ? 'X' : cell}
+module TicTacToe
+    $LINES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+    $board = [[1,2,3], [4,5,6], [7,8,9]]
+    class Game
+        def print_board
+            @@col_seperator, @@row_seperator = ' | ', '--+---+----'
+            puts $board.map { |row| row.join(@@col_seperator) }.join("\n" + @@row_seperator + "\n")
         end
-        puts played_board.map { |row| row.join(col_seperator) }.join("\n" + row_seperator + "\n")
-        play_count += 1
-        player += 1
-        if (LINES.any? do |line|
-            line.all? {|position| positions_1.include?(position)}
-            end)
-            puts 'Player1 won'
-            isGameOver = true
+        attr_reader :board
+        def initialize
+            @@isGameOver = false
+            @@play_count = 0
+            @@played_board = $board
+            @@player = 1
+            @@positions_1 = []
+            @@positions_2 = []
         end
-    elsif player == 2
-        begin
-        puts 'Player 2 Please Enter The Number To Mark'
-        choice_2 = gets.chomp.to_i
-        end while (positions_1+positions_2).include?(choice_2)
-        positions_2 << choice_2
-        p positions_2
-        played_board = played_board.map do |row| 
-            row.map {|cell| cell == choice_2&&cell != 'X'&&cell != 'O' ? 'O' : cell}
+        def play
+            print_board
+            initialize
+            while !@@isGameOver && @@play_count < 9 do
+                if @@player == 1
+                    choice_1 = player_selection(@@positions_1+@@positions_2)
+                    mark_board(choice_1, 'X')
+                    @@positions_1 << choice_1
+                    if player_has_won?(@@positions_1)
+                        puts 'Player1 won'
+                        @@isGameOver = true
+                    end
+                    @@player += 1
+                elsif @@player == 2
+                    choice_2 = player_selection(@@positions_1+@@positions_2)
+                    mark_board(choice_2, 'O')
+                    @@positions_2 << choice_2
+                    if player_has_won?(@@positions_2)
+                        puts 'Player2 won'
+                        @@isGameOver = true
+                    end
+                    @@player -= 1
+                end
+            end
+            if @@play_count = 9 && !@@isGameOver
+                puts 'The game is draw!!'
+            end
         end
-        puts played_board.map { |row| row.join(col_seperator) }.join("\n" + row_seperator + "\n")
-        play_count += 1
-        player -= 1 
-        if (LINES.any? do |line|
-            line.all? {|position| positions_2.include?(position)}
-            end)
-            puts 'Player2 won'
-            isGameOver = true
+        def player_has_won?(marks)
+            return $LINES.any? do |line|
+                line.all? {|position| marks.include?(position)}
+            end
+        end
+        def player_selection(positions)
+            begin
+                puts "Player #{@@player} Please Enter The Number To Mark"
+                selection = gets.chomp.to_i
+            end while (positions).include?(selection) 
+            return selection
+        end
+        def mark_board(choice, mark)
+            @@played_board = @@played_board.map do |row| 
+                row.map {|cell| cell == choice ? mark : cell}
+            end
+            puts @@played_board.map { |row| row.join(@@col_seperator) }.join("\n" + @@row_seperator + "\n")
+            @@play_count += 1
         end
     end
-    
 end
 
-
-class Player1
-    
-end
-
-class Player2
-
-end
+include TicTacToe
+Game.new.play
